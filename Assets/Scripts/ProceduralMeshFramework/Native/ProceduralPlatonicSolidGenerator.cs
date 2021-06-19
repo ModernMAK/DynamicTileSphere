2745db0306f5474671810e7302fb15dadf225886
Marcus Kertesz <ModernMAK@Hotmail.com>
2021-06-18 23:26:59 -0700

    Cube

--- Assets/Scripts/ProceduralMeshFramework/Native/ProceduralPlatonicSolidGenerator.cs
+++ Assets/Scripts/ProceduralMeshFramework/Native/ProceduralPlatonicSolidGenerator.cs
@@ -401,6 +401,83 @@ namespace ProceduralMeshFramework.NNatives
         /// <returns></returns>
         public static PositionGraph BuildGraph() => SharedUtil.BuildGraph(Vertexes, Indexes, NodeEdges, PolyEdges, Twins);
     }
+    public static class Cube
+    {
+        private const float
+            SQRT_2 = SharedUtil.SQRT_2,
+            SQRT_3 = SharedUtil.SQRT_3;
+
+        //ALL Shapes have a circumradius of 1, to convert to their original circumradius, multiply by the circumradius, to conver to their mid or inradius, divide by the circumradius (when at 1), then multiply by the inradius 
+        //Dont know what these are? Look at this (https://en.wikipedia.org/wiki/Platonic_solid) bout midway down the page
+        public const float
+            CircumRadius = SQRT_3,
+            MidRadius = SQRT_2,
+            InRadius = 1f;
+        public static float GetRadius(RadiusType radius)
+        {
+            switch (radius)
+            {
+                case RadiusType.Circumradius:
+                    return CircumRadius;
+                case RadiusType.Midradius:
+                    return MidRadius;
+                case RadiusType.Inradius:
+                    return InRadius;
+                case RadiusType.Normalized:
+                    return 1f;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(radius));
+            }
+        }
+
+        public const int PolyCount = 6;
+        public const int SoftVertCount = 8;
+        public const int PolyVertCount = 4;
+        private static readonly float3[] Vertexes = new float3[SoftVertCount]
+        {
+            math.normalize(new float3(1f, 1f, 1f)),
+            math.normalize(new float3(1f, 1f, -1f)),
+            math.normalize(new float3(1f, -1f, 1f)),
+            math.normalize(new float3(1f, -1f, -1f)),
+            math.normalize(new float3(-1f, 1f, 1f)),
+            math.normalize(new float3(-1f, 1f, -1f)),
+            math.normalize(new float3(-1f, -1f, 1f)),
+            math.normalize(new float3(-1f, -1f, -1f))
+        };
+        /// <summary>
+        /// 4 Triangles ~ [PolygonIndex, VertexIndex]
+        /// Order matters if converting to mesh, not converting to graph
+        /// </summary>
+        /// <remarks>Changing this will require Twins, NodeEdges, & PolyEdges to be updated.</remarks>
+        private static readonly int[,] _Indexes = new int[PolyCount, PolyVertCount]
+        {
+            // Right
+            {2, 1, 0, 3},
+            // Top
+            {5, 4, 0, 1 },
+            //Forward
+            {1, 3, 7, 5},
+            //Left
+            {4, 5, 7, 6 },
+            //Down
+            {2, 6, 7, 3 },
+            //Back
+            {4, 2, 0, 6 }
+        };
+        private static readonly int[] Indexes = SharedUtil.Flatten(_Indexes);
+
+        //Hardcoded to avoid writing an algorithm for something that could be calculated once; this does mean this breaks if we change the INDEXES
+        private static readonly int[] Twins = SharedUtil.CalculateTwins(Indexes, PolyCount);
+        private static readonly int[] NodeEdges = SharedUtil.CalculateNodeEdges(Indexes, PolyCount);
+        private static readonly int[] PolyEdges = SharedUtil.CalculatePolyEdges(PolyVertCount, PolyCount);
+
+
+        /// <summary>
+        /// Builds a soft graph.
+        /// </summary>
+        /// <returns></returns>
+        public static PositionGraph BuildGraph() => SharedUtil.BuildGraph(Vertexes, Indexes, NodeEdges, PolyEdges, Twins);
+    }
 
     public static class ProceduralPlatonicSolidGenerator
     {
@@ -416,20 +493,6 @@ namespace ProceduralMeshFramework.NNatives
             
 
 
-        
-
-
-        private static readonly float3[] CubeVerticies = new float3[8]
-        {
-            math.normalize(new float3(1f, 1f, 1f)),
-            math.normalize(new float3(1f, 1f, -1f)),
-            math.normalize(new float3(1f, -1f, 1f)),
-            math.normalize(new float3(1f, -1f, -1f)),
-            math.normalize(new float3(-1f, 1f, 1f)),
-            math.normalize(new float3(-1f, 1f, -1f)),
-            math.normalize(new float3(-1f, -1f, 1f)),
-            math.normalize(new float3(-1f, -1f, -1f))
-        };
 
         private static readonly float3[] IcosahedronVerticies = new float3[12]
         {
@@ -475,49 +538,6 @@ namespace ProceduralMeshFramework.NNatives
             math.normalize(new float3(-PHI, 0f, INV_PHI)), //18
             math.normalize(new float3(-PHI, 0f, -INV_PHI)) //19            
         };
-
-
-        private static readonly ProceduralMeshBuilder CubePrototype = BuildCubePrototype();
-
-        private static readonly ProceduralMeshBuilder IcosahedronPrototype = BuildIcosahedronPrototype();
-
-        private static readonly ProceduralMeshBuilder DodecahedronPrototype = BuildDodecahedronPrototype();
-
-        private static ProceduralMeshBuilder BuildCubePrototype()
-        {
-            var verts = CubeVerticies;
-            var builder = new ProceduralMeshBuilder();
-            var rotation = Quaternion.identity;
-            var vertexCount = verts.Length;
-
-            for (var i = 0; i < vertexCount; i++)
-            {
-                var modifiedVertex = ModifyVertex(verts[i], rotation, false);
-                builder.AddVertex(modifiedVertex);
-            }
-
-            //Bot
-            builder.AddTriangle(2, 1, 0);
-            builder.AddTriangle(1, 2, 3);
-            //For
-            builder.AddTriangle(5, 4, 0);
-            builder.AddTriangle(1, 5, 0);
-            //Right
-            builder.AddTriangle(1, 3, 7);
-            builder.AddTriangle(7, 5, 1);
-            //Top
-            builder.AddTriangle(4, 5, 7);
-            builder.AddTriangle(7, 6, 4);
-            //Back
-            builder.AddTriangle(2, 6, 7);
-            builder.AddTriangle(2, 7, 3);
-            //Left
-            builder.AddTriangle(4, 2, 0);
-            builder.AddTriangle(2, 4, 6);
-
-
-            return builder;
-        }
 
         private static ProceduralMeshBuilder BuildIcosahedronPrototype()
         {
