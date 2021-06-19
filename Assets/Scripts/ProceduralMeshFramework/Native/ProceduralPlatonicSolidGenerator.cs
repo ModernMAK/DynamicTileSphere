8577e799811d81caa4d49acbea924a4c13f00e0e
Marcus Kertesz <ModernMAK@Hotmail.com>
2021-06-18 23:03:15 -0700

    Update ProceduralPlatonicSolidGenerator.cs

--- Assets/Scripts/ProceduralMeshFramework/Native/ProceduralPlatonicSolidGenerator.cs
+++ Assets/Scripts/ProceduralMeshFramework/Native/ProceduralPlatonicSolidGenerator.cs
@@ -1,16 +1,158 @@
 ﻿using Graphing.Position.Generic.Native;
 using Graphing.Position.Native;
 using System;
+using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
 
 namespace ProceduralMeshFramework.NNative
 {
 
-    public static class RadiusShapeUtil 
+    public static class SharedUtil 
     {
+        public static int[] CalculateTwins(IList<int> Indexes, int PolyCount)
+        {
+            int PolyVertCount = Indexes.Count / PolyCount;
+            var twins = new int[Indexes.Count];
+            for (var i = 0; i < twins.Length; i++)
+                twins[i] = -1; // Init to -1 to allow skipping on 0
 
-        private const float
+            // This algo is a mess; but simply put
+            //  ~ ITERATE OVER EACH POLYGON (PolyA)
+            //      ~ ITERATE OVER EACH EDGE (PolyA Edge) ... we use current and next index of the triangle as an 'edge'
+            //      > SKIP IF EDGE HAS TWIN           
+            //      > FIND TWIN
+            //          ~ ITERATE OVER EACH POLY (PolyB) ...skip PolyA so PolyA != PolyB
+            //              ~ ITERATE OVER EACH EDGE (PolyB Edge)
+            //              > SKIP IF EDGE HAS TWIN
+            //                  ~ COMPARE EDGE 
+            //                  > If Found Go to next PolyA Edge (break)
+
+            for (var i = 0; i < PolyCount; i++)
+            {
+                var iOffset = i * PolyVertCount;
+                for (var j = 0; j < PolyVertCount; j++)
+                {
+                    if (twins[iOffset + j] != -1)
+                        continue;
+
+                    var jNext = (j + 1) % PolyVertCount;
+
+                    var a = Indexes[iOffset + j];
+                    var b = Indexes[iOffset + jNext];
+
+                    var found = false;
+                    for (var k = 0; k < PolyCount && k != i && !found; k++)
+                    {
+                        var kOffset = k * PolyVertCount;
+                        for (var l = 0; l < PolyVertCount && !found; l++)
+                        {
+                            if (twins[kOffset + l] != -1)
+                                continue;
+
+                            var lNext = (l + 1) % PolyVertCount;
+
+                            var c = Indexes[kOffset + l];
+                            var d = Indexes[kOffset + lNext];
+
+                            if (a == d && b == c)
+                            {
+                                twins[iOffset + j] = kOffset + l;
+                                twins[kOffset + l] = iOffset + j;
+                                found = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            return twins;
+        }
+
+        public static int[] CalculatePolyEdges(int PolyVertCount, int PolyCount)
+        {
+            var polys = new int[PolyCount];
+            for (var i = 0; i < polys.Length; i++)
+                polys[i] = PolyVertCount * i;
+            return polys;
+        }
+        public static int[] CalculateNodeEdges(IList<int> Indexes, int SoftVertexCount)
+        {
+            var nodes = new int[SoftVertexCount];
+            var found = 0;
+            for (var i = 0; i < nodes.Length; i++)
+                nodes[i] = -1;
+            for (var i = 0; i < Indexes.Count && found < SoftVertexCount; i++)
+			{
+                var index = Indexes[i];
+                if (nodes[index] == -1)
+                {
+                    nodes[index] = i;
+                    found++;
+                }
+			}
+            return nodes;
+        }
+        public static int[] Flatten(int[,] arr)
+		{
+            var u = arr.GetLength(0);
+            var v =arr.GetLength(1);
+            var flat = new int[u * v];
+            for(var i = 0; i < flat.Length; i++)
+			{
+                var j = i / u;
+                var k = i % u;
+                flat[i] = arr[j, k];
+			}
+            return flat;
+		}
+
+        public static PositionGraph BuildGraph(IList<float3> Vertexes, IList<int> Indexes, IList<int> Twins, IList<int> NodeEdges, IList<int> PolyEdges)
+        {
+            var PolyCount = PolyEdges.Count;
+            var PolyVertCount = Indexes.Count / PolyCount;
+
+            var graph = new PositionGraph(Vertexes.Count, PolyCount * PolyVertCount, PolyCount);
+            //Initilaize Vertexes
+            for (var i = 0; i < Vertexes.Count; i++)
+            {
+                var node = graph.Nodes[i];
+                node.Data = new PositionData { Position = Vertexes[i] };
+                node.Edge = NodeEdges[i];
+                graph.Nodes[i] = node;
+            }
+            // Initilalize Polygons & Edges
+            for (var polyIndex = 0; polyIndex < PolyCount; polyIndex++)
+            {
+                var poly = graph.Polygons[polyIndex];
+                poly.Edge = PolyEdges[polyIndex];
+                graph.Polygons[polyIndex] = poly;
+
+
+                var edgeOffset = polyIndex * PolyVertCount;
+                //Initialize Edges
+                for (var edgeIndex = 0; edgeIndex < PolyVertCount; edgeIndex++)
+                {
+                    var edge = graph.Edges[edgeOffset + edgeIndex];
+
+                    edge.Next = edgeOffset + (edgeIndex + 1) % PolyVertCount;
+                    edge.Prev = edgeOffset + (edgeIndex + PolyVertCount - 1) % PolyVertCount;
+
+                    var fullIndex = polyIndex * PolyVertCount + edgeIndex;
+
+                    edge.Twin = Twins[fullIndex];
+                    edge.Node = Indexes[fullIndex];
+
+                    edge.Poly = polyIndex;
+
+                    graph.Edges[edgeOffset + edgeIndex] = edge;
+                }
+
+            }
+            return graph;
+        }
+
+        public const float
             PHI = 1.61803398875f,
 
             //These 4 numbers are used for the Cirucm/Mid/In radius stuff
@@ -120,12 +262,15 @@ namespace ProceduralMeshFramework.NNative
         }
     }
 
+    
+
+
     public static class Tetrahedron
     {
         private const float
-            SQRT_6 = 2.44948974278f,
-            SQRT_2 = 1.41421356237f,
-            SQRT_3 = 1.73205080757f;
+            SQRT_6 = SharedUtil.SQRT_6,
+            SQRT_2 = SharedUtil.SQRT_2,
+            SQRT_3 = SharedUtil.SQRT_3;
 
         //ALL Shapes have a circumradius of 1, to convert to their original circumradius, multiply by the circumradius, to conver to their mid or inradius, divide by the circumradius (when at 1), then multiply by the inradius 
         //Dont know what these are? Look at this (https://en.wikipedia.org/wiki/Platonic_solid) bout midway down the page
@@ -145,7 +290,7 @@ namespace ProceduralMeshFramework.NNative
                 case RadiusType.Inradius:
                     return InRadius;
                 case RadiusType.Normalized:
-                    return 1f;
+                    return 1f;//This is always 1 because the array is normalized.
                 default:
                     throw new ArgumentOutOfRangeException(nameof(radius));
             }
@@ -165,71 +310,24 @@ namespace ProceduralMeshFramework.NNative
         /// 4 Triangles ~ [PolygonIndex, VertexIndex]
         /// Order matters if converting to mesh, not converting to graph
         /// </summary>
-        /// <remarks>Changing this will require Twins, NodeEdges, & PolyEdges to be updated.</remarks>
-        private static readonly int[,] Indexes = new int[PolyCount, PolyVertCount]
+        private static readonly int[,] _Indexes = new int[PolyCount, PolyVertCount]
         {
             { 0, 1, 2 },
             { 3, 1, 0 },
             { 0, 2, 3 },
             { 3, 2, 1 },
         };
-
-        //Hardcoded to avoid writing an algorithm for something that could be calculated once; this does mean this breaks if we change the INDEXES
-        private static readonly int[,] Twins = new int[PolyCount, PolyVertCount]
-        {
-            { 4, 10, 6 },
-            { 11, 0, 8 },
-            { 2, 9, 5 },
-            { 7, 1, 3 },
-        };
-        private static readonly int[] NodeEdges = new int[SoftVertCount] { 0, 1, 2, 3 }; // Not all edges will be this cut and dry
-        private static readonly int[] PolyEdges = new int[SoftVertCount] { 0, 3, 6, 9 }; // Not all edges will be this cut and dry
+        private static readonly int[] Indexes = SharedUtil.Flatten(_Indexes);
+        private static readonly int[] Twins = SharedUtil.CalculateTwins(Indexes, PolyCount);
+        private static readonly int[] NodeEdges = SharedUtil.CalculateNodeEdges(Indexes, SoftVertCount);
+        private static readonly int[] PolyEdges = SharedUtil.CalculatePolyEdges(PolyVertCount, PolyCount);
 
 
         /// <summary>
         /// Builds a soft graph.
         /// </summary>
         /// <returns></returns>
-        public static PositionGraph BuildGraph()
-		{
-
-            var graph = new PositionGraph(Vertexes.Length, PolyCount * PolyVertCount, PolyCount);
-            //Initilaize Vertexes
-            for(var i = 0; i < Vertexes.Length; i++)
-			{
-                var node = graph.Nodes[i];
-                node.Data = new PositionData { Position = Vertexes[i] };
-                node.Edge = NodeEdges[i];
-                graph.Nodes[i] = node;
-            }
-            // Initilalize Polygons & Edges
-            for (var polyIndex = 0; polyIndex < PolyCount; polyIndex++)
-            {
-                var poly = graph.Polygons[polyIndex];
-                poly.Edge = PolyEdges[polyIndex];
-                graph.Polygons[polyIndex] = poly;
-
-
-                var edgeOffset = polyIndex * PolyVertCount;
-                //Initialize Edges
-                for (var edgeIndex = 0; edgeIndex < PolyVertCount; edgeIndex++)
-                {
-                    var edge = graph.Edges[edgeOffset + edgeIndex];
-                    
-                    edge.Next = edgeOffset + (edgeIndex + 1) % PolyVertCount;
-                    edge.Prev = edgeOffset + (edgeIndex + PolyVertCount - 1) % PolyVertCount;
-                    
-                    edge.Twin = Twins[polyIndex, edgeIndex];
-                    edge.Node = Indexes[polyIndex, edgeIndex];
-                    
-                    edge.Poly = polyIndex;
-
-                    graph.Edges[edgeOffset + edgeIndex] = edge;
-                }
-
-            }
-            return graph;
-		}
+        public static PositionGraph BuildGraph() => SharedUtil.BuildGraph(Vertexes, Indexes, Twins, NodeEdges, PolyEdges);
     }
 
     public static class ProceduralPlatonicSolidGenerator
